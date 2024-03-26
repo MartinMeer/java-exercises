4ff7c6547339d7e9cc46e25d9c66f2e81e714a49
ОТК <olegmms@inbox.ru>
2024-03-26 17:37:10 +0300

    fix: exit

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -6,32 +6,38 @@ import java.util.Scanner;
 public class EvenOdd {
 
     private static Random random = new Random();
-    private static int randomNumber = random.nextInt(99);
+    //private static int randomNumber = random.nextInt(99);
     private static Scanner scanner = new Scanner(System.in);
 
 
     public static void playEvenOdd() {
-        int counter = 1;
-        while (counter < 4) {
-            int number = randomNumber;
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        int counter = 0;
+        while (counter < 3) {
+            int randomNumber = random.nextInt(99);
             String correctAnswer = checkNumber(randomNumber);
-            System.out.println("Question: " +
-                    number +
+            System.out.print("Question: " +
+                    randomNumber +
                     "\nYour answer: ");
             String answer = scanner.nextLine();
             if (answer.equalsIgnoreCase(correctAnswer)) {
-                counter++;
+                System.out.println("Correct!");
+                counter += 1;
+            } else {
+                System.out.println("\'" + answer + "\' is wrong answer ;(. Correct answer was \'" + correctAnswer + "\'.\n" +
+                        "Let's try again, NULL!");
             }
-            System.out.println("\'" +answer+ "\' is wrong answer ;(. Correct answer was \'" +correctAnswer+ "\'.\n" +
-                    "Let's try again, Bill!");
+            System.exit(0);
         }
     }
-    private static String checkNumber(int number) {
-        if ((number % 2) == 0) {
-        return "yes";
-        }else {return "no";
+        private static String checkNumber(int number){
+            if ((number % 2) == 0) {
+                return "yes";
+            } else {
+                return "no";
         }
     }
-
-
 }
+
+
+
